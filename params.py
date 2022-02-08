adb0f28baf10c54643d52f2c5fcc349c70a7512e
pujangga123 <rush_x@yahoo.com>
2022-02-09 00:35:09 +0700

    2022-02-08

--- params.py
+++ params.py
@@ -1,2 +1,10 @@
-def print_test():
-    print("test")
+import sys
+
+class Params:
+    paramlist = []
+    
+    def __init__(self) -> None:
+        self.paramlist = sys.argv
+
+    def get(self,index):
+        return self.paramlist[index] if index<len(self.paramlist) else None
